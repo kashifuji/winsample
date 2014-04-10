b55cdd558bdcbe5c58d4623a46c31f98aacc403f
kashifuji <katashiyo515@yahoo.co.jp>
2014-04-10 22:39:01 +0900

    test

--- Sample/MainPage.xaml.cs
+++ Sample/MainPage.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Xml.Dom;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +27,40 @@ namespace Sample
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            test();
+            //this.Frame.Navigate(typeof(BlankPage1), null);
+        }
+        
+        private async void test() {
+            Uri uri = new Uri("http://localhost:3000/con1/form2");
+            var client = new HttpClient();
+            var postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("user", "test"));
+            HttpContent content = new FormUrlEncodedContent(postData);
+
+            try
+            {
+//                HttpResponseMessage response = await client.GetAsync(uri);
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+                string str = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine("response body is [" + str);
+
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(str);
+                XmlElement elem = doc.DocumentElement;
+                XmlNodeList oldNode = elem.GetElementsByTagName("old");
+                System.Diagnostics.Debug.WriteLine("Name; " + oldNode[0].InnerText);  
+            }
+            catch { }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
